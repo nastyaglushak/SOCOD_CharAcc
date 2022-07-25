26ecec69eeeed5d5ae9e1f7e43f1b04c9f41517c
Anastasia Glushak <55274993+nastyaglushak@users.noreply.github.com>
2022-07-26 00:08:46 +0700

    Update socod.cpp

--- socod.cpp
+++ socod.cpp
@@ -12,29 +12,29 @@ SOCOD::~SOCOD(){
 }
 void SOCOD::GetData(vector<int>& acquiredDataVec){
     string line;
-    //acquiredData->clear();
+    acquiredData->clear();
 	ifstream ifile("test.txt", ios::in);
 	if (ifile.is_open()){
 		while (ifile>>line){
-			//acquiredData->push_back(stof(line));
+			acquiredData->push_back(stof(line));
             acquiredDataVec.push_back(stof(line));
 		}		
 	}
 	ifile.close();
 }
-vector<int>* SOCOD::GetAccData(){
-    return acquiredData;
-}
 vector<int> SOCOD::ProcessData(const vector<int>& acquiredDataVec){
-    /*for (int i=0; i<acquiredData.size(); i=i+4){
-        processData->push_back(acquiredData[i]);
-    }*/
+    for (int i=0; i<acquiredData->size(); i=i+4){
+        processData->push_back(acquiredData->at(i));
+    }
     for (int i=0; i<acquiredDataVec.size(); i=i+4){
         processDataVec.push_back(acquiredDataVec[i]);
     }
     return processDataVec;
 }
 
+vector<int>* SOCOD::GetAccData(){
+    return acquiredData;
+}
 vector<int>* SOCOD::GetProcessData(){
     return processData;
 }
