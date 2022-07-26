0e4da08dcef5ff12df109c8b5b3f4d9c30f4782d
Anastasia Glushak <55274993+nastyaglushak@users.noreply.github.com>
2022-07-26 22:57:24 +0700

    Update socod.cpp

--- socod.cpp
+++ socod.cpp
@@ -1,8 +1,8 @@
 #include "socod.hpp"
 
 SOCOD::SOCOD(){
-    acquiredData=new vector<int>(channelNum*frameNum);
-    processData=new vector<int>(channelNum*frameNum/4);
+    acquiredData=new vector<int>(THRNum*chNum);
+    processData=new vector<int>(chNum);
     cout<<"SOCOD Constructor"<<endl;
 }
 SOCOD::~SOCOD(){
@@ -10,31 +10,43 @@ SOCOD::~SOCOD(){
     delete processData;
     cout<<"SOCOD Destructor"<<endl;
 }
-void SOCOD::GetData(vector<int>& acquiredDataVec){
-    string line;
+void SOCOD::GetRandomData(){
     acquiredData->clear();
-	ifstream ifile("test.txt", ios::in);
+    for (int i=0; i != THRNum*chNum; ++i){
+        acquiredData->push_back(1+rand()/((RAND_MAX + 1u)/10));
+    }
+}
+
+void SOCOD::GetDataFromFile(){
+    std::string line;
+    acquiredData->clear();
+	std::ifstream ifile("test.txt", std::ios::in);
 	if (ifile.is_open()){
 		while (ifile>>line){
 			acquiredData->push_back(stof(line));
-            acquiredDataVec.push_back(stof(line));
 		}		
 	}
 	ifile.close();
 }
-vector<int> SOCOD::ProcessData(const vector<int>& acquiredDataVec){
+void SOCOD::ProcessData(){
+    processData->clear();
     for (int i=0; i<acquiredData->size(); i=i+4){
+        //cout<<"i "<<i<<" "<<acquiredData->at(i)<<endl;
         processData->push_back(acquiredData->at(i));
     }
-    for (int i=0; i<acquiredDataVec.size(); i=i+4){
-        processDataVec.push_back(acquiredDataVec[i]);
-    }
-    return processDataVec;
 }
-
 vector<int>* SOCOD::GetAccData(){
     return acquiredData;
 }
 vector<int>* SOCOD::GetProcessData(){
     return processData;
+}
+void SOCOD::DataVectorOut(){
+    cout<<"Data Vector Out "<<endl;
+    std::copy(acquiredData->begin(), acquiredData->end(),ostream_iterator<int>(cout, " "));
+    cout<<endl;
+
+    cout<<"Data Vector Out after Process"<<endl;
+    std::copy(processData->begin(), processData->end(),ostream_iterator<int>(cout, " "));
+    cout<<endl;
 }
