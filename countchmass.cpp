df6d5f6a0836b1dc3ac641bb49f3035a4229aff0
Anastasia Glushak <55274993+nastyaglushak@users.noreply.github.com>
2022-07-31 18:54:06 +0700

    Update countchmass.cpp

--- countchmass.cpp
+++ countchmass.cpp
@@ -1,52 +1,79 @@
 #include"countchmass.hpp"
 #include"socod.hpp"
 
-CountCharMass::CountCharMass(int ChNum, int CntNum){
+CountCharMass::CountCharMass(int FrNum, int CntNum){
     cout<<"Class Constructor"<<endl;
     this->socod=new SOCOD();
-    this->ChNum=ChNum;
+    this->FrNum=FrNum;
     this->CntNum=CntNum;
-    inData=new vector<int>(CntNum);
+    inData=new vector<int>(socod->GetChNum());
+    //accumData=new vector<int>(CntNum*(socod->GetChNum()));
+    accumData=new vector<int>();
+    oneAccumData=new vector<int>(socod->GetChNum()); 
 }
 
 CountCharMass::~CountCharMass(){
     delete inData;
+    delete accumData;
+    delete oneAccumData;
     cout<<"Class Destructor"<<endl;
 }
 
-void CountCharMass::DataSocodShow(){
-    cout<<"SOCOD Data"<<endl;
+void CountCharMass::GetDataSocod(){
+    inData->clear();
+    //cout<<"SOCOD Data"<<endl;
     socod->GetRandomData();
     socod->ProcessData();
-    socod->DataVectorOut();
-    cout<<endl;
+    //socod->DataVectorOut();
+    inData=socod->GetProcessData();
+
 }
-vector<int> CountCharMass::DataAccum(const vector<int>& dataVec1) {
-    for(size_t i=0; i<dataVec1.size(); ++i){
-        cout<<dataVec1[i]<<" "<<inValData[i]<<" "<<i<<endl;
-        inValData[i]+=dataVec1[i];
+void CountCharMass::CreateZeroVector(){
+    oneAccumData->clear();
+    for(size_t i=0; i<socod->GetChNum();++i){
+        oneAccumData->push_back(0);
     }
-    return inValData;
+}
+void CountCharMass::OneDataAccum(){
+    CreateZeroVector();
+    for (size_t k=0; k<FrNum;++k){
+        GetDataSocod();
+        //cout<<"Data From Socod"<<endl;
+        for(size_t i=0; i<inData->size();++i){
+            oneAccumData->at(i)+=inData->at(i); 
+            //cout<<oneAccumData->at(i)<<" "<<inData->at(i)<<" "<<i<<endl; 
+        }
+        //cout<<"Number of Iter "<<k<<endl;
+        //ShowData();
+    }
 }
 
-void CountCharMass::DataVectorOut(){
-    cout<<"Data Vector Out"<<endl;
-    copy(inValData.begin(), inValData.end(),ostream_iterator<int>(cout, " "));
-    cout<<endl;
+void CountCharMass::DataAccum(){
+    for (size_t i=0; i<CntNum; ++i){
+        OneDataAccum();
+        for (size_t k=0; k<socod->GetChNum(); ++k){
+           accumData->push_back(oneAccumData->at(k)); 
+        }
+        cout<<"Number of Iter "<<i<<endl;
+        ShowData();
+    }
+}
 
-    cout<<"Data Vector Out (Show Adress)"<<endl;
+void CountCharMass::ShowData(){
+    cout<<"Input Data"<<endl;
     copy(inData->begin(), inData->end(),ostream_iterator<int>(cout, " "));
     cout<<endl;
+
+    cout<<"One String Accum Data"<<endl;
+    copy(oneAccumData->begin(), oneAccumData->end(),ostream_iterator<int>(cout, " "));
+    cout<<endl;
+
+    cout<<"Final Accum Data"<<endl;
+    copy(accumData->begin(), accumData->end(),ostream_iterator<int>(cout, " "));
+    cout<<endl;
 }
 
-void CountCharMass::DataAnalyse(vector<int>& dataVec){
-    for (const int& s: dataVec){
-        inData->push_back(s);
-        inValData.push_back(s);
-    }
-}
-
-vector<int>* CountCharMass::GetData(){
-    return inData;
+vector<int>* CountCharMass::GetAccumData(){
+    return accumData;
 }
 
